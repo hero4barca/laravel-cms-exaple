f0f433e652deeee2f8657d0b76494ae2cd9dac6e
Hero <hero@pop-os.localdomain>
2021-03-02 17:24:00 +0100

    updating api routes

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::apiResource('companies', 'App\Http\Controllers\CompanyController');
+Route::apiResource('employees', 'App\Http\Controllers\EmployeeController');
+Route::apiResources(['users' => 'App\Http\Controllers\UserController'], ['except' => ['delete']]);
